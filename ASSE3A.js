ef68f90ba827841c43ca8317fd334b25f26f96cf
Cyllindra <Heber.Nelski@gmail.com>
2023-10-12 11:36:42 -0700

    CSS for web pages, and js files for ASSE3A

---
+++ ASSE3A.js
@@ -0,0 +1,34 @@
+x = Math.ceil(Math.random()*20-10);
+y = Math.ceil(Math.random()*20-10);
+
+if (x <= 0){x = x - 1;}
+if (y <= 0){y = y - 1;}
+
+b = x + y;
+c = x * y;
+
+ax = Math.abs(x);
+ay = Math.abs(y);
+ab = Math.abs(b);
+ac = Math.abs(c);
+
+sign1 = " + ";
+sign2 = " + ";
+
+if (b < 0){
+  sign1 = " - ";
+}
+if (c < 0){
+  sign2 = " - ";
+}
+
+if (b == 0){poly = "x<sup>2</sup>" + sign2 + c;}
+else{poly = "x<sup>2</sup>" + sign1 + ab + "x" + sign2 + ac;}
+
+sign1 = " + ";
+sign2 = " + ";
+
+if (x < 0){sign1 = " - ";}
+if (y < 0){sign2 = " - ";}
+
+ans = "(x" + sign1 + ax + ")(x" + sign2 + ay + ")";
