df79153f391918feece6c07725d81e320f0fa5c5
BetaPictoris <beta@hai.haus>
2023-10-17 10:54:32 -0700

    Added checking for the answer.

--- scripts/ASSE3A.js
+++ scripts/ASSE3A.js
@@ -28,4 +28,33 @@ poly = `x^2 ${display_b} ${display_c} = 0`;
 
 console.log("Polynomial: " + poly);
 
-ans = "x = " + x*-1 + ", " + y*-1;
+ans_1 = x*-1;
+ans_2 = y*-1;
+
+console.log("Answer 1: " + ans_1);
+console.log("Answer 2: " + ans_2);
+
+function check() {
+  val_1 = document.getElementById("val_1").value;
+  val_2 = document.getElementById("val_2").value;
+
+  if (val_1 == ans_1 || val_1 == ans_2) {
+    if (val_2 == ans_1 && val_1 == ans_2) {
+      correctDisplay();
+    } else if (val_1 == ans_1 && val_2 == ans_2) {
+      correctDisplay();
+    } else {
+      incorrectDisplay();
+    }
+  } else {
+    incorrectDisplay();
+  }
+}
+
+function correctDisplay() {
+  document.getElementById("result").innerHTML = "<span style='color: green; font-style: italic;'>Correct!</span> Click <a href='ASSE3A.html'>here</a> for a new problem.";
+}
+
+function incorrectDisplay() {
+  document.getElementById("result").innerHTML = "<span style='color: red; font-style: italic;'>Incorrect!</span>";
+}
