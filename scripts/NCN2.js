00d51b913b3157f174be41e7b899e03268e39658
BetaPictoris <beta@hai.haus>
2023-10-17 11:29:26 -0700

    Remove Roboto

--- scripts/NCN2.js
+++ scripts/NCN2.js
@@ -1,15 +1,25 @@
-a = Math.ceil(Math.random()*20-10);
-b = Math.ceil(Math.random()*20-10);
-c = Math.ceil(Math.random()*20-10);
-d = Math.ceil(Math.random()*20-10);
+a = Math.ceil(Math.random() * 20 - 10);
+b = Math.ceil(Math.random() * 20 - 10);
+c = Math.ceil(Math.random() * 20 - 10);
+d = Math.ceil(Math.random() * 20 - 10);
 
-bsign = " + ";
-dsign = " + ";
+bsign = "+";
+dsign = "+";
 
-if (b <= 0){b = b - 1; bsign = " - ";}
-if (c <= 0){d = d - 1; dsign = " - ";}
+if (b <= 0) {
+  b = b - 1;
+  bsign = "-";
+}
+if (c <= 0) {
+  d = d - 1;
+  dsign = "-";
+}
 
-ab = Math.abs(b)
-ad = Math.abs(d)
+ab = Math.abs(b);
+ad = Math.abs(d);
 
-numbers = a + bsign + b + "i and " + c + dsign + d + "i"
+number_a = a + bsign + ab + "i";
+number_b = c + dsign + ad + "i";
+
+console.log(number_a);
+console.log(number_b);
