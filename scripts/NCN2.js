c08678afb7c5a1c7355c85d17f69e4cdc57a3134
Cyllindra <Heber.Nelski@gmail.com>
2023-10-16 17:42:14 -0700

    Update NCN2.js

--- scripts/NCN2.js
+++ scripts/NCN2.js
@@ -6,8 +6,8 @@ d = Math.ceil(Math.random()*20-10);
 bsign = " + ";
 dsign = " + ";
 
-if (b <= 0){a = a - 1; asign = " - ";}
-if (c <= 0){c = c - 1; csign = " - ";}
+if (b <= 0){b = b - 1; bsign = " - ";}
+if (c <= 0){d = d - 1; dsign = " - ";}
 
 ab = Math.abs(b)
 ad = Math.abs(d)
