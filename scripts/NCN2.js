0037e799ae157a58f8269d925cfb4df8fb5739ed
BetaPictoris <beta@hai.haus>
2023-10-17 17:08:50 -0700

    Added answer check

--- scripts/NCN2.js
+++ scripts/NCN2.js
@@ -21,5 +21,36 @@ ad = Math.abs(d);
 number_a = a + bsign + ab + "i";
 number_b = c + dsign + ad + "i";
 
-console.log(number_a);
-console.log(number_b);
+sign = " + ";
+
+if (a * d + b * c <= 0) {
+  sign = " - ";
+}
+
+real = a * c - b * d;
+imag = Math.abs(a * d + b * c);
+
+ans = real + sign + imag + "i";
+
+function check() {
+  val_1 = document.getElementById("val_1").value;
+  val_2 = document.getElementById("val_2").value;
+
+  console.log(`${val_1} + ${val_2}i`);
+
+  if (val_1 == real && val_2 == imag) {
+    correctDisplay();
+  } else {
+    incorrectDisplay();
+  }
+}
+
+function correctDisplay() {
+  document.getElementById("result").innerHTML =
+    "<span style='color: green; font-style: italic;'>Correct!</span> Click <a href='NCN2.html'>here</a> for a new problem.";
+}
+
+function incorrectDisplay() {
+  document.getElementById("result").innerHTML =
+    "<span style='color: red; font-style: italic;'>Incorrect!</span>";
+}
