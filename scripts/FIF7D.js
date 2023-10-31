09cf8a8f1e079ca080a50a5778f84a19f60b3dab
Cyllindra <Heber.Nelski@gmail.com>
2023-10-31 10:21:22 -0700

    Attempt to fix latex

--- scripts/FIF7D.js
+++ scripts/FIF7D.js
@@ -22,8 +22,8 @@ if (az > z){zsign = `-`;}
 if (ar > z){rsign = `-`;}
 if (an > z){nsign = `-`;}
 
-top = `\\((x ${zsign} ${az})(x ${rsign} ${ar})\\)`;
-bottom = `\\((x ${nsign} ${an})(x ${rsign} ${ar})\\)`;
+top = `(x ${zsign} ${az})(x ${rsign} ${ar})`;
+bottom = `(x ${nsign} ${an})(x ${rsign} ${ar})`;
 
 tp = z + r;
 tq = z * r;
@@ -45,6 +45,6 @@ if (atq > tq){tqsign = `-`;}
 if (abp > bp){bpsign = `-`;}
 if (abq > bq){bqsign = `-`;}
 
-rational = `\\($\dfrac{x^2 ${tpsign} ${atp}x ${tqsign} ${atq}}{x^2 ${bpsign} ${abp}x ${bqsign} ${abq}}\\)`;
+rational = `\dfrac{x^2 ${tpsign} ${atp}x ${tqsign} ${atq}}{x^2 ${bpsign} ${abp}x ${bqsign} ${abq}}`;
 
 ans = `There are two holes -- a removable hole at ${r}, and a non-removable hole (or vertical asymptote) at ${n}$.`;
