7f60b4651b744d4513692b5f532b28131f7b819f
Cyllindra <Heber.Nelski@gmail.com>
2023-10-31 08:49:57 -0700

    Update FIF7D.js

--- scripts/FIF7D.js
+++ scripts/FIF7D.js
@@ -14,16 +14,16 @@ az = Math.abs(z);
 ar = Math.abs(r);
 an = Math.abs(n);
 
-zsign = "+";
-rsign = "+";
-nsign = "+";
+zsign = `+`;
+rsign = `+`;
+nsign = `+`;
 
-if (az > z){zsign = "-";}
-if (ar > z){rsign = "-";}
-if (an > z){nsign = "-";}
+if (az > z){zsign = `-`;}
+if (ar > z){rsign = `-`;}
+if (an > z){nsign = `-`;}
 
-top = "(x ${zsign} ${az})(x ${rsign} ${ar})";
-bottom = "(x ${nsign} ${an})(x ${rsign} ${ar})";
+top = `(x ${zsign} ${az})(x ${rsign} ${ar})`;
+bottom = `(x ${nsign} ${an})(x ${rsign} ${ar})`;
 
 tp = z + r;
 tq = z * r;
@@ -35,16 +35,16 @@ atq = Math.abs(tq);
 abp = Math.abs(bp);
 abq = Math.abs(bq);
 
-tpsign = "+";
-tqsign = "+";
-bpsign = "+";
-bqsign = "+";
+tpsign = `+`;
+tqsign = `+`;
+bpsign = `+`;
+bqsign = `+`;
 
-if (atp > tp){tpsign = "-";}
-if (atq > tq){tqsign = "-";}
-if (abp > bp){bpsign = "-";}
-if (abq > bq){bqsign = "-";}
+if (atp > tp){tpsign = `-`;}
+if (atq > tq){tqsign = `-`;}
+if (abp > bp){bpsign = `-`;}
+if (abq > bq){bqsign = `-`;}
 
-rational = "$\dfrac{x^2 ${tpsign} ${atp}x ${tqsign} ${atq}}{x^2 ${bpsign} ${abp}x ${bqsign} ${abq}}";
+rational = `$\dfrac{x^2 ${tpsign} ${atp}x ${tqsign} ${atq}}{x^2 ${bpsign} ${abp}x ${bqsign} ${abq}}`;
 
-ans = "There are two holes -- a removable hole at ${r}, and a non-removable hole (or vertical asymptote) at ${n}$.";
+ans = `There are two holes -- a removable hole at ${r}, and a non-removable hole (or vertical asymptote) at ${n}$.`;
