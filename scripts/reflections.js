99b8ac6f847cc846c92dae4577ba4af471333a4b
BetaPictoris <beta@hai.haus>
2023-11-05 15:29:52 -0800

    Update reflections to the new style.

--- scripts/reflections.js
+++ scripts/reflections.js
@@ -1,50 +1,46 @@
-x1 = Math.ceil(Math.random()*20-10);
-y1 = Math.ceil(Math.random()*20-10);
+x1 = Math.ceil(Math.random() * 20 - 10);
+y1 = Math.ceil(Math.random() * 20 - 10);
 p1 = "(" + x1 + ", " + y1 + ")";
 
-x2 = Math.ceil(Math.random()*20-10);
-y2 = Math.ceil(Math.random()*20-10);
+x2 = Math.ceil(Math.random() * 20 - 10);
+y2 = Math.ceil(Math.random() * 20 - 10);
 p2 = "(" + x2 + ", " + y2 + ")";
 
-x3 = Math.ceil(Math.random()*20-10);
-y3 = Math.ceil(Math.random()*20-10);
+x3 = Math.ceil(Math.random() * 20 - 10);
+y3 = Math.ceil(Math.random() * 20 - 10);
 p3 = "(" + x3 + ", " + y3 + ")";
 
 points = p1 + ", " + p2 + ", and " + p3;
-r = Math.ceil(Math.random()*20-10);
+r = Math.ceil(Math.random() * 20 - 10);
 
-vert = false
+vert = false;
 svalue = "x";
 dvalue = "y";
 
-ans = "Ref(x, y) = (";
+ans = "\\left(";
 
-if (Math.ceil(Math.random()*20-10) > 0)
-{
+if (Math.ceil(Math.random() * 20 - 10) > 0) {
   vert = true;
 }
 
-if (vert)
-{
-  nx1 = 2*r - x1;
-  nx2 = 2*r - x2;
-  nx3 = 2*r - x3;
+if (vert) {
+  nx1 = 2 * r - x1;
+  nx2 = 2 * r - x2;
+  nx3 = 2 * r - x3;
   ny1 = y1;
   ny2 = y2;
   ny3 = y3;
-  ans = ans + 2*r + " - x, y)";
+  ans = ans + 2 * r + " - x, y\\right)";
   svalue = "y";
   dvalue = "x";
-}
-else
-{
-  ny1 = 2*r - y1;
-  ny2 = 2*r - y2;
-  ny3 = 2*r - y3;
+} else {
+  ny1 = 2 * r - y1;
+  ny2 = 2 * r - y2;
+  ny3 = 2 * r - y3;
   nx1 = x1;
   nx2 = x2;
   nx3 = x3;
-  ans = ans + "x, " + 2*r + " - y)";
+  ans = ans + "x, " + 2 * r + " - y\\right)";
 }
 np1 = "(" + nx1 + ", " + ny1 + ")";
 np2 = "(" + nx2 + ", " + ny2 + ")";
