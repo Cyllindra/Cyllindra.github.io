b03d1098fa968894289c1f6a6e5f9350c6547b9d
Cyllindra <Heber.Nelski@gmail.com>
2023-11-07 11:45:36 -0800

    Update GSRT8A.js

--- scripts/GSRT8A.js
+++ scripts/GSRT8A.js
@@ -1,12 +1,12 @@
 angle = Math.ceil(Math.random()*71+9);
 adj = Math.ceil(Math.random()*26+4);
 
-angle = angle * Math.PI / 180;
+radians = angle * Math.PI / 180;
 
-opp = adj * Math.tan(angle);
+opp = adj * Math.tan(radians);
 opp = opp.toFixed(2);
 
-hyp = adj / Math.cos(angle);
+hyp = adj / Math.cos(radians);
 hyp = hyp.toFixed(2);
 
 tangent = `tan(\\theta)=\\dfrac{${opp}}{${adj}}`;
